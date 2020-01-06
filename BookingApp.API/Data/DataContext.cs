7597821c0f1ceeb44fe23dd42e18cdcb0f743d9c
Kelvin Cortez <kelvincortez@Kelvins-iMac.local>
2020-01-06 14:57:38 +1300

    Initial Commit

---
+++ BookingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using BookingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
