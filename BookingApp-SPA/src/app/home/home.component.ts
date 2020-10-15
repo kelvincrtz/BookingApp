8aae386723519b7c9b22592f50589f0f9e42d92a
Kelvin Cortez <kelvincortez@Kelvins-iMac.local>
2020-10-15 17:31:50 +1300

    Front-end: General Update

--- BookingApp-SPA/src/app/home/home.component.ts
+++ BookingApp-SPA/src/app/home/home.component.ts
@@ -43,6 +43,10 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  cancelLogin() {
+    this.loginForm.reset(this.user);
+  }
+
   registerToggle() {
     this.registerMode = true;
   }
